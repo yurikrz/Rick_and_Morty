b8e9ba8f4a4dc1d257f7bc2445f6f5f314fdb428
Yurik Rivera <yurikrz@hotmail.com>
2023-10-09 22:22:48 -0600

    version7

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
   //[background:_linear-gradient(180deg,rgba(0,0,0,0.00)_13.02%,_#000_100%),_url("/bg2.webp"),_lightgray_0%_0%_/_50px_50px_repeat]
   //className='px-4 min-h-screen text-white'
   return (
-    <main className='min-h-screen text-white relative max-w-[1020px] mx-auto px-3 min-w-[400px]'>
+    <main className='min-h-screen text-white relative max-w-[1060px] mx-auto px-3 min-w-[400px]'>
       <div className='fixed top-0 left-0 w-full h-full [background:_linear-gradient(180deg,rgba(0,0,0,0.6)_0%,rgba(0,0,0,0.0)_100%),_url("/bg5.png"),_lightgray_0px_-770.693px_/_100%_166.7%_no-repeat] bg-[cover] bg-[50%] z-[-1] after:fixed after:top-0 after:left-0 after:w-full after:h-full sm:[[background:_linear-gradient(180deg,rgba(0,0,0,0.00)_13.02%,_#000_100%),_url("/bg5.png"),_lightgray_0%_0%_/_50px_50px_repeat]]'>
       </div>
       <div className='relative z-20 h-28 sm:h-36 w-full p-5 '>
